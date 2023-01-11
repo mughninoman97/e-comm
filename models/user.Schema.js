50f207a2cb27158fa2a9405edfbd8b0e503dabc6
Mohd Abdul Mughni Noman <84847023+mughninoman97@users.noreply.github.com>
2023-01-11 12:09:19 +0000

    module created

---
+++ models/user.Schema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema({
+    
+    name:{
+        type:String,
+        required:[true, "name is required"],
+        maxLength: [50, 'name must be less than 50 char']
+    },
+    email: {
+        type:String,
+        required:[true, 'email is requred']
+
+    }
+})
