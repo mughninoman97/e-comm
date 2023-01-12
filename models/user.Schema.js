65be66826f8e9dd420b97a6d1003683f00d89d4d
Mohd Abdul Mughni Noman <84847023+mughninoman97@users.noreply.github.com>
2023-01-12 08:47:45 +0000

    added functionality methods in userschema

--- models/user.Schema.js
+++ models/user.Schema.js
@@ -3,6 +3,7 @@ import AuthRoles from "../util/authRoles"
 import bcrypt from 'bcryptjs'
 import JWT from 'jsonwebtoken'
 import crypto from 'crypto'
+import config from '../config/index'
 
 const userSchema = mongoose.Schema(
     {
@@ -47,13 +48,37 @@ const userSchema = mongoose.Schema(
 
 // challege 1- encrypt the password
     userSchema.pre('save', async function(next){
-        if(this.modified("password")){
-            this.password = encryptedPassword;
+        if(!this.modified("password")){
+            this.password = encryptedPassword()
         }
 
         if(!this.modified("password")) return next();
         this.password = await bcrypt.hash(this.password,10)
         next()
     })
+// functionality-comp pwd, jwt, 
+userSchema.methods = {
+    // feature1- compare password
+    comparePassword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password)
+    },
+    // generate jwt token 
+    getJwtToken: function(){
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn: config.JWT_EXPIRY
+
+            }
+            
+        )
+    }
+}    
+// getneate jwt token 
+
 
 export default mongoose.model("User", userSchema)
