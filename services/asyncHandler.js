3f9b0b0fd9632f100ca9f6d238a4c63494bcee7b
Mohd Abdul Mughni Noman <84847023+mughninoman97@users.noreply.github.com>
2023-01-28 16:41:50 +0000

    added synchandler

---
+++ services/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (fn) => async (req,res,next) => {
+
+    try {
+        await fn(req,res,next)    
+    } catch (error) {
+        res.status(error.code || 500 ).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+
+export default asyncHandler;
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async() => {}
