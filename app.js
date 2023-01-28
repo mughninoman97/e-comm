8d5a9df995e9a49d90a8120fe62e5865e27af774
Mohd Abdul Mughni Noman <84847023+mughninoman97@users.noreply.github.com>
2023-01-28 18:15:30 +0000

    morgan & express middleware addrd

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
 
+//middlewares 
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+app.use(cookieParser)
+
+//morgan logger
+app.use(morgan('tiny'))
 
 export default app
