d738b0e9a18261d37c60e26fdd95733e9673d391
Mohd Abdul Mughni Noman <84847023+mughninoman97@users.noreply.github.com>
2023-02-01 08:48:34 +0000

    mail utility added

--- config/index.js
+++ config/index.js
@@ -7,7 +7,12 @@ const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
     MONGODB_URL: process.env.MONGODB_URL,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    SMTP_MAIL_HOST:process.env.SMTP_MAIL_HOST,
+    SMTP_MAIL_HOST:process.env.PORT,
+    SMTP_MAIL_HOST:process.env.USERNAME,
+    SMTP_MAIL_HOST:process.env.SMTP_MAIL_PASSWORD,
+    SMTP_MAIL_EMAIL:process.env.SMTP_MAIL_EMAIL
 
 }
 
